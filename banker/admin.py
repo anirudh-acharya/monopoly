b6fd9e96eaa5264c16962716a81978fffe91b0cf
Anirudh Acharya <acharyaani92@gmail.com>
2017-04-25 09:19:45 +0530

    Add Game,Player,Account models, and migrations.

--- banker/admin.py
+++ banker/admin.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Transaction
+from .models import Game,Player,Account,Transaction
 
 
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Account)
 admin.site.register(Transaction)
--- banker/models.py
+++ banker/models.py
@@ -5,12 +5,39 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
+class Player(models.Model):
+    person = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.person.__str__()
+
+
+class Game(models.Model):
+    players = models.ManyToManyField(Player, through='Account')
+    start_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Account(models.Model):
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(Player)
+    balance = models.IntegerField()
+
+    class Meta:
+        unique_together = ('game', 'player',)
+
+    def __str__(self):
+        return "Game: %s Player: %s" % (str(self.game), str(self.player))
+
+
 class Transaction(models.Model):
-    payer = models.ForeignKey(User, related_name='payer')
-    payee = models.ForeignKey(User, related_name='payee')
+    payer_account = models.ForeignKey(Account, related_name='payer_account')
+    payee_account = models.ForeignKey(Account, related_name='payee_account')
     amount = models.PositiveIntegerField()
     description = models.CharField(max_length=255)
-
 
     def __str__(self):
         return "%s paid %s rupees to %s. Remarks: %s" % (
@@ -18,3 +45,4 @@ class Transaction(models.Model):
                 str(self.amount),
                 self.payee.first_name,
                 self.description)
+
