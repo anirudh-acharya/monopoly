9f1099c22c0d9548ba4a8def88bd62e99c24e089
Anirudh Acharya <acharyaani92@gmail.com>
2017-05-18 15:49:09 +0530

    Add working test case for Transaction save signal
    The post_save signal emitted by django is connected to the method to
    update the account balances of the payer and the payee involved in the
    transaction.
    This modifiction itself is done using F expression in django, which is
    supposed to be more robuts that directly assigning a new value for the
    field and calling save on the model for writing the changes to database.
    
    A big assumption here is that the transaction added once can not be
    removed, as there is no implementation for the update of the transaction
    as of now. There are some possibilities of hooking up this with the
    pre_save signal, and fetching the old value of the balance from the db,
    and then adjusting the accounts' balance accordingly, but that requires
    custom code in the model afaik.

--- banker/signals.py
+++ banker/signals.py
@@ -2,15 +2,29 @@
 from __future__ import unicode_literals
 
 from django.db.models.signals import post_save
+from django.db.models import F
 from django.dispatch import receiver
 
-from .models import Transaction
+from banker.models import Transaction
 
 
 @receiver(post_save, sender=Transaction)
-def update_account_balance(sender, **kwargs):
+def update_account_balance_before_transaction_save(sender, **kwargs):
     """
     Updates the account balance of the payer and the payee of the transaction
     """
-    print kwargs
+
+    transaction = kwargs['instance']
+
+    payer_account = transaction.payer_account
+    payee_account = transaction.payee_account
+
+    payer_account.balance = F('balance') - transaction.amount
+    payee_account.balance = F('balance') + transaction.amount
+
+    payer_account.save()
+    payee_account.save()
+
+    payer_account.refresh_from_db()
+    payee_account.refresh_from_db()
 
--- banker/tests/test_signals.py
+++ banker/tests/test_signals.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
 
 from django.test import TestCase
+from django.contrib.auth.models import User
 from banker.models import Game, Player, Account, Transaction
-
-from banker import signals
 
 
 class SignalTestCases(TestCase):
     def setUp(self):
         game = Game.objects.create()
 
+        person_one = User.objects.create(id=1, username='person_one')
+        person_two = User.objects.create(id=2, username='person_two')
+
         player_one = Player.objects.create(person_id=1)
         player_two = Player.objects.create(person_id=2)
 
-        account_one = Account.objects.create(game=game,
+        self.account_one = Account.objects.create(game=game,
                 player=player_one,
                 balance=1500)
 
-        account_two = Account.objects.create(game=game,
+        self.account_two = Account.objects.create(game=game,
                 player=player_two,
                 balance=1500)
 
-        transaction = Transaction.objects.create(payer_account=player_one,
-                payee_account=player_two,
+        self.transaction = Transaction.objects.create(payer_account=self.account_one,
+                payee_account=self.account_two,
                 amount = 200,
                 description = 'test transaction'
                 )
 
-    def test_update_account_balance(self):
-        update_accout_balance(Transaction.__class__,
-                instance=transaction,
-                create=True,
-                raw=False,
-                using='default',
-                update_fields=None
-                )
+    def test_update_account_balance_signal_for_transaction_creation(self):
+        self.assertEquals(self.account_one.balance, 1300)
+        self.assertEquals(self.account_two.balance, 1700)
 
+    def test_update_account_balance_signal_for_transaction_update(self):
+        self.transaction.amount = 100
+        self.transaction.save()
+
+        self.assertEquals(self.account_one.balance, 1200)
+        self.assertEquals(self.account_two.balance, 1800)
