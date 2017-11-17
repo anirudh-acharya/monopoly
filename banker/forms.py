32e1a8b1dca16273269adda333bec105837e1a54
Anirudh Acharya <acharyaani92@gmail.com>
2017-11-17 20:30:39 +0000

    Fixed passing game_id to TransactionForm

--- banker/forms.py
+++ banker/forms.py
@@ -9,8 +9,9 @@ class TransactionForm(forms.ModelForm):
         model = Transaction
         fields = '__all__'
 
-    def __init__(self, game_id=None, **kwargs):
-        super(TransactionForm, self).__init__(**kwargs)
+    def __init__(self, game_id=None, *args, **kwargs):
+        super(TransactionForm, self).__init__(*args, **kwargs)
+
         if game_id:
             self.fields['payer_account'].queryset = Account.objects.filter(game_id=game_id)
             self.fields['payee_account'].queryset = Account.objects.filter(game_id=game_id)
