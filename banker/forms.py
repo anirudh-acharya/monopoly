3eb47624712dffdf02fb792e0331e18191e29439
Anirudh Acharya <acharyaani92@gmail.com>
2017-11-23 20:40:26 +0000

    Account select as radio list, cleaned up css
    Reordered the balance. and recent transaction tables
    Chagned the widget used to display the accounts to radio select

--- banker/forms.py
+++ banker/forms.py
@@ -8,6 +8,13 @@ class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = '__all__'
+        error_css_class = 'error'
+        required_css_class = 'required'
+
+        widgets = {
+                'payer_account': forms.RadioSelect,
+                'payee_account': forms.RadioSelect,
+            }
 
     def __init__(self, game_id=None, *args, **kwargs):
         super(TransactionForm, self).__init__(*args, **kwargs)
